4b0de5456d60bf7d86118c582d1cb8ecc9c1b4b7
Adrian Jabłonka <jablonka.adrian@gmail.com>
2017-10-28 01:15:57 +0200

    Login i kilka poprawek

---
+++ Assets/Application/Scripts/LoadScene.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class LoadScene : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    LoadScene(string sceneName) {
+        SceneManager.LoadScene(sceneName);
+    }
+}
